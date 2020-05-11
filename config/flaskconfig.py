89c2509f1bae4810059a7d296eecd3ad69e0c2a7
LSQI15 <siqi.li15@gmail.com>
2020-05-11 11:07:56 -0500

    update configuration file

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -13,9 +13,19 @@ DEBUG = True
 
 # Local Sqlite Database Connection Config
 DATABASE_PATH = os.path.join(PROJECT_ROOT, 'data/customer.db')
-LOCAL_DATABASE_URI = 'sqlite:////{}'.format(DATABASE_PATH)
+LOCAL_ENGINE_STRING = 'sqlite:////{}'.format(DATABASE_PATH)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
 
 # RDS Database Connection Config
+# the engine_string format
+# engine_string = "{conn_type}://{user}:{password}@{host}:{port}/{database}"
+CONN_TYPE = "mysql+pymysql"
+USER = os.environ.get("MYSQL_USER")
+PASSWORD = os.environ.get("MYSQL_PASSWORD")
+RDS_HOST = os.environ.get("MYSQL_HOST")
+RDS_PORT = os.environ.get("MYSQL_PORT")
+DATABASE_NAME = os.environ.get("MYSQL_DATABASE_NAME")
+RDS_ENGINE_STRING = "{}://{}:{}@{}:{}/{}".\
+format(CONN_TYPE, USER, PASSWORD, RDS_HOST, RDS_PORT, DATABASE_NAME)
 
