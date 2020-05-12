db05ea31035ab147e9cc757213ef8782fe1de2f9
LSQI15 <siqi.li15@gmail.com>
2020-05-12 11:41:30 -0500

    added download_data parser

--- run.py
+++ run.py
@@ -1,23 +1,30 @@
 import argparse
 
 from src.create_db import create_db
-from src.upload_download_data import upload_data
+from src.upload_download_data import upload_data, download_data
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Run Components of Model Source Code")
     subparsers = parser.add_subparsers()
 
-    # Sub-parser for creating a database
+    # Sub-parser for creating a database to store user input
     sb_create_db = subparsers.add_parser("create_db", description="Creating the Databse")
-    sb_create_db.add_argument('--rds', default=False, help='Option to use RDS or not')
+    sb_create_db.add_argument('--rds', default="False", help='Option to use RDS or not')
     sb_create_db.set_defaults(func=create_db)
+
+    # Sub-parser for downloading raw data from s3 bucket
+    sb_download = subparsers.add_parser("download_data", description="Download data from S3 bucket")
+    sb_download.add_argument('--local_file_path', help="Local folder where downloaded data will be saved")
+    sb_download.add_argument('--file_name', help="File name of the data file")
+    sb_download.add_argument('--source_url', help="AWS S3 bucket where the data is stored")
+    sb_download.set_defaults(func=download_data)
 
     # Sub-parser for uploading the data to S3
     sb_upload = subparsers.add_parser("upload_data", description="Upload data into S3")
-    sb_upload.add_argument('--localfolder', help="Local folder containing data to be uploaded")
-    sb_upload.add_argument('--filename', help="File name of the data file")
-    sb_upload.add_argument('--bucket', help="AWS S3 bucket where the data will be stored")
+    sb_upload.add_argument('--local_file_path', help="Local folder containing data to be uploaded")
+    sb_upload.add_argument('--file_name', help="File name of the data file")
+    sb_upload.add_argument('--bucket_url', help="AWS S3 bucket where the data will be stored")
     sb_upload.set_defaults(func=upload_data)
 
     args = parser.parse_args()
