898315578193db158f2ceacd3077140feb3bfb4a
LSQI15 <siqi.li15@gmail.com>
2020-05-28 22:31:14 -0500

    update data_download function with yaml config

--- src/upload_download_data.py
+++ src/upload_download_data.py
@@ -1,15 +1,11 @@
 import boto3
-import requests
 import yaml
-import argparse
-import os
-
 import logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("down_load_data")
 
 def upload_data(args):
     """
-    Upload data in the local folder to a S3 bucket of user's choice
+    function to data in the local folder to a S3 bucket of user's choice
     :param args: The name of S3 bucket which data will be uploaded to
         local_file_path: path to the saved file
         file_name: the name of the file
@@ -21,25 +17,34 @@ def upload_data(args):
         path = args.local_file_path + '/' + args.file_name
         s3.upload_file(path, args.bucket_name, 'data/%s' % args.file_name)
         logger.info("%s is successfully uploaded to bucket %s" % (args.file_name, args.bucket_name))
-    except boto3.exceptions.S3UploadFailedError:
+    except:
         logger.error("Error: upload unsuccessful.")
 
 
 def download_data(args):
     """
-    Download data from a public S3 bucket to the local folder based on user input
-    :param args
-        .bucket_name (String): name of the S3 bucket; default bucket is 'msia423-customer-churn'
-        .s3_file: file to be downloaded; default is 'data/raw_data.csv'
-        .local_file_path (String): path to the saved file
-        .file_name (String): name of the file
+    main function to download the raw data from a s3 bucket
+    :param args (argparse): user-input configuration file
+    """
+    with open(args.config, "r") as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+    data_downloader(**config['run_download_data']['download_data'])
+
+
+def data_downloader(bucket_name,s3_file, file_path,file_name):
+    """
+    Helper function to data from a S3 bucket to the local folder based on user input
+    :param bucket_name: name of the S3 bucket; default bucket is
+    :param s3_file: name of the file to be downloaded
+    :param file_path: path to the saved file
+    :param file_name: name of the saved file
     :return: None
     """
-    path = args.local_file_path + '/' + args.file_name
     try:
+        path = file_path + '/' + file_name
         s3 = boto3.client('s3')
-        s3.download_file(args.bucket_name, args.s3_file, path)
-        logger.info("%s is downloaded from S3 bucket %s to %s " % (args.s3_file, args.bucket_name, path))
-    except requests.exceptions.RequestException:
-        logger.error("Error: Unable to download %s from S3 bucket %s" % (args.s3_file, args.bucket_name))
+        s3.download_file(bucket_name, s3_file, path)
+        logger.info("%s is downloaded from S3 bucket %s to %s " % (s3_file, bucket_name, path))
+    except:
+        logger.error("Error: Unable to download %s from S3 bucket %s" % (s3_file, bucket_name))
 
