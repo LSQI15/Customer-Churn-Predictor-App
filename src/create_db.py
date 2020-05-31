a8ae8adf23f56db95b8f366eb8a94f9ceccddb25
LSQI15 <siqi.li15@gmail.com>
2020-05-30 23:18:44 -0500

    update functions with yaml config; change database schema; added function for doing initial ingestion.

--- src/create_db.py
+++ src/create_db.py
@@ -1,10 +1,14 @@
-import os
-import sys
-import logging
 import sqlalchemy as sql
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String, Text, Float, MetaData
 import logging.config
+import yaml
+import pandas as pd
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.orm import sessionmaker
+from src.helper import csv_reader
+from src.churn_predictor import load_model, make_ingest_df
+
+
 import config.flaskconfig as conf
 
 logger = logging.getLogger(__name__)
@@ -16,26 +20,28 @@ class Customer(Base):
     """
     __tablename__ = 'customer'
 
-    id = Column(Integer, primary_key=True)                              # auto-increment id
-    Gender = Column(Integer, unique=False, nullable=False)              # binary (0/1)
-    SeniorCitizen = Column(Integer, unique=False, nullable=False)       # binary (0/1)
-    Partner = Column(Integer, unique=False, nullable=False)             # binary (0/1)
-    Dependents = Column(Integer, unique=False, nullable=False)          # binary (0/1)
-    Tenure = Column(Integer, unique=False, nullable=False)              # integer
-    PhoneService = Column(Integer, unique=False, nullable=False)        # binary (0/1)
-    MultipleLines = Column(Integer, unique=False, nullable=False)       # categorical with 3 classes (0/1/2)
-    InternetService = Column(String(100), unique=False, nullable=False)  # categorical - company names
-    OnlineSecurity = Column(Integer, unique=False, nullable=False)      # categorical with 3 classes (0/1/2)
-    OnlineBackup = Column(Integer, unique=False, nullable=False)        # categorical with 3 classes (0/1/2)
-    DeviceProtection = Column(Integer, unique=False, nullable=False)    # categorical with 3 classes (0/1/2)
-    TechSupport = Column(Integer, unique=False, nullable=False)         # categorical with 3 classes (0/1/2)
-    StreamingTV = Column(Integer, unique=False, nullable=False)         # categorical with 3 classes (0/1/2)
-    StreamingMovies = Column(Integer, unique=False, nullable=False)     # categorical with 3 classes (0/1/2)
-    Contract = Column(String(100), unique=False, nullable=False)         # categorical - contact type
-    PaperlessBilling = Column(Integer, unique=False, nullable=False)    # binary (0/1)
-    PaymentMethod = Column(String(100),  unique=False, nullable=False)   # categorical - payment type
-    MonthlyCharges = Column(sql.Float, unique=False, nullable=False)    # double
-    TotalCharges = Column(sql.Float, unique=False, nullable=False)      # double
+    id = Column(Integer, primary_key=True)                               # auto-increment id
+    Gender = Column(String(100), unique=False, nullable=False)          # binary (Yes/No)
+    SeniorCitizen = Column(String(100), unique=False, nullable=False)    # binary (Yes/No)
+    Partner = Column(String(100), unique=False, nullable=False)          # binary (Yes/No)
+    Dependents = Column(String(100), unique=False, nullable=False)       # binary (Yes/No)
+    Tenure = Column(Float, unique=False, nullable=False)                 # double
+    PhoneService = Column(String(100), unique=False, nullable=False)     # binary (Yes/No)
+    MultipleLines = Column(String(100), unique=False, nullable=False)    # categorical with 3 classes ('No phone service', 'No', 'Yes')
+    InternetService = Column(String(100), unique=False, nullable=False)  # categorical with 3 classes ('DSL', 'Fiber optic', 'No')
+    OnlineSecurity = Column(String(100), unique=False, nullable=False)   # binary (Yes/No)
+    OnlineBackup = Column(String(100), unique=False, nullable=False)     # binary (Yes/No)
+    DeviceProtection = Column(String(100), unique=False, nullable=False) # binary (Yes/No)
+    TechSupport = Column(String(100), unique=False, nullable=False)      # binary (Yes/No)
+    StreamingTV = Column(String(100), unique=False, nullable=False)      # binary (Yes/No)
+    StreamingMovies = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    Contract = Column(String(100), unique=False, nullable=False)         # categorical with 3 classes ('Month-to-month', 'One year', 'Two year')
+    PaperlessBilling = Column(String(100), unique=False, nullable=False) # binary (Yes/No)
+    PaymentMethod = Column(String(100), unique=False, nullable=False)    # categorical with 4 classes ('Electronic check', 'Mailed check','Bank transfer (automatic)','Credit card (automatic)'
+    MonthlyCharges = Column(Float, unique=False, nullable=False)         # double
+    TotalCharges = Column(Float, unique=False, nullable=False)           # double
+    PredProbability = Column(Float, unique=False, nullable=False)        # double
+    PredClass = Column(String(100), unique=False, nullable=False)        # binary (Yes/No)
 
     def __repr__(self):
         return "<Customer(id='%d', monthly_charges='%d', total_charges='%d')>" \
@@ -44,17 +50,36 @@ class Customer(Base):
 
 def create_db(args):
     """
-        Create the database locally or in RDS based on user input
-        :param args: True/False indicating whether to create the database in RDS
-        :return: None
+    Create the database locally or in RDS
+    :param args: Argparse args - should include args.SQLALCHEMY_DATABASE_URI
+    :return: None
     """
-    if args.rds == "True":      # set up the database in RDS
-        engine_string = conf.RDS_ENGINE_STRING
-        print(engine_string)
-    elif args.rds == "False":   # set up the database locally
-        engine_string = conf.LOCAL_ENGINE_STRING
-    else:
-        raise logger.error("Invalid args.rds option %s: " % args.rds)
-    # create engine and database
-    engine = sql.create_engine(engine_string, echo = True)
-    Base.metadata.create_all(engine)
+    try:
+        engine = sql.create_engine(args.engine_string, echo = True)
+        Base.metadata.create_all(engine)
+        logger.info("Database has been successfully created at %s" % args.engine_string)
+    except:
+        logger.error("Error: unable to create database at %s" % args.engine_string)
+
+
+def initial_ingest(args):
+    """
+    intitial ingest - ingest the database with 10 observations
+    :param args: Argparse args - should include args.title, args.artist, args.album
+    :return: None
+    """
+    try:
+        with open(args.config, "r") as f:
+            config = yaml.load(f, Loader=yaml.SafeLoader)
+        # load the random forest model
+        rf = load_model(**config['run_create_db']['init_ingest'])
+        # treat the first 10 rows of the preprocessed data as initial records
+        df = csv_reader(**config['run_create_db']['init_df']).drop('Churn', axis=1).head(10)
+        # make a data frame that is ready for ingestion
+        ingest_df = make_ingest_df(rf, df)
+        # add customers to the database.
+        ingest_df.to_sql(con=args.engine_string, index=False, name=Customer.__tablename__, if_exists='append')
+        logger.info('Successfully conducted initial ingestion to the database.')
+    except:
+        logger.error('Error: unable to conduct initial ingestion to the database')
+
