60d7dcd026d312a5fe5e167e8df8a498f88df58d
LSQI15 <siqi.li15@gmail.com>
2020-06-05 13:59:25 -0500

    revise initial_ingest(args) function. users can now specify hwo many records they want to ingest initially

--- src/customer_db.py
+++ src/customer_db.py
@@ -9,34 +9,39 @@ from src.helper import csv_reader, load_model, make_ingest_df
 logger = logging.getLogger(__name__)
 Base = declarative_base()
 
+
 class Customer(Base):
     """ Defines the data model for the table `customer`.
         This table will store user inputs based on which prediction will be made
     """
     __tablename__ = 'customer'
 
-    id = Column(Integer, primary_key=True)                               # auto-increment id
-    Gender = Column(String(100), unique=False, nullable=False)          # binary (Yes/No)
-    SeniorCitizen = Column(String(100), unique=False, nullable=False)    # binary (Yes/No)
-    Partner = Column(String(100), unique=False, nullable=False)          # binary (Yes/No)
-    Dependents = Column(String(100), unique=False, nullable=False)       # binary (Yes/No)
-    Tenure = Column(Float, unique=False, nullable=False)                 # double
-    PhoneService = Column(String(100), unique=False, nullable=False)     # binary (Yes/No)
-    MultipleLines = Column(String(100), unique=False, nullable=False)    # categorical with 3 classes ('No phone service', 'No', 'Yes')
-    InternetService = Column(String(100), unique=False, nullable=False)  # categorical with 3 classes ('DSL', 'Fiber optic', 'No')
-    OnlineSecurity = Column(String(100), unique=False, nullable=False)   # binary (Yes/No)
-    OnlineBackup = Column(String(100), unique=False, nullable=False)     # binary (Yes/No)
-    DeviceProtection = Column(String(100), unique=False, nullable=False) # binary (Yes/No)
-    TechSupport = Column(String(100), unique=False, nullable=False)      # binary (Yes/No)
-    StreamingTV = Column(String(100), unique=False, nullable=False)      # binary (Yes/No)
+    id = Column(Integer, primary_key=True)  # auto-increment id
+    Gender = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    SeniorCitizen = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    Partner = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    Dependents = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    Tenure = Column(Float, unique=False, nullable=False)  # double
+    PhoneService = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    MultipleLines = Column(String(100), unique=False,
+                           nullable=False)  # categorical with 3 classes ('No phone service', 'No', 'Yes')
+    InternetService = Column(String(100), unique=False,
+                             nullable=False)  # categorical with 3 classes ('DSL', 'Fiber optic', 'No')
+    OnlineSecurity = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    OnlineBackup = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    DeviceProtection = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    TechSupport = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    StreamingTV = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
     StreamingMovies = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
-    Contract = Column(String(100), unique=False, nullable=False)         # categorical with 3 classes ('Month-to-month', 'One year', 'Two year')
-    PaperlessBilling = Column(String(100), unique=False, nullable=False) # binary (Yes/No)
-    PaymentMethod = Column(String(100), unique=False, nullable=False)    # categorical with 4 classes ('Electronic check', 'Mailed check','Bank transfer (automatic)','Credit card (automatic)'
-    MonthlyCharges = Column(Float, unique=False, nullable=False)         # double
-    TotalCharges = Column(Float, unique=False, nullable=False)           # double
-    PredProbability = Column(Float, unique=False, nullable=False)        # double
-    PredClass = Column(String(100), unique=False, nullable=False)        # binary (Yes/No)
+    Contract = Column(String(100), unique=False,
+                      nullable=False)  # categorical with 3 classes ('Month-to-month', 'One year', 'Two year')
+    PaperlessBilling = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
+    PaymentMethod = Column(String(100), unique=False,
+                           nullable=False)  # categorical with 4 classes ('Electronic check', 'Mailed check','Bank transfer (automatic)','Credit card (automatic)'
+    MonthlyCharges = Column(Float, unique=False, nullable=False)  # double
+    TotalCharges = Column(Float, unique=False, nullable=False)  # double
+    PredProbability = Column(Float, unique=False, nullable=False)  # double
+    PredClass = Column(String(100), unique=False, nullable=False)  # binary (Yes/No)
 
     def __repr__(self):
         return "<Customer(id='%d', monthly_charges='%d', total_charges='%d')>" \
@@ -50,7 +55,7 @@ def create_db(args):
     :return: None
     """
     try:
-        engine = sql.create_engine(args.engine_string, echo = True)
+        engine = sql.create_engine(args.engine_string, echo=True)
         Base.metadata.create_all(engine)
         logger.info("Database has been successfully created at %s" % args.engine_string)
     except:
@@ -73,8 +78,9 @@ def ingest_db(df, engine_string):
 
 def initial_ingest(args):
     """
-    main function to conduct initial ingestion to the database
-    :param args: Argparse args - should include args.engine_string, args.config
+    main function to conduct initial ingestion to the database; will ingest the database with args.num_records
+    observations;
+    :param args: Argparse args - should include args.engine_string, args.config, args.num_records
     :return: None
     """
     try:
@@ -82,12 +88,11 @@ def initial_ingest(args):
             config = yaml.load(f, Loader=yaml.SafeLoader)
         # load the random forest model
         rf = load_model(**config['run_create_db']['init_ingest'])
-        # add all existing records in the preprocessed data to the database
-        df = csv_reader(**config['run_create_db']['init_df']).drop('Churn', axis=1)
+        # add existing records from the preprocessed data to the database
+        df = csv_reader(**config['run_create_db']['init_df']).drop('Churn', axis=1).head(args.num_records)
         # make a data frame that is ready for ingestion
         ingest_df = make_ingest_df(rf, df)
         # add customers to the database.
         ingest_db(ingest_df, args.engine_string)
     except:
         logger.error('Error: unable to conduct initial ingestion to the database')
-
